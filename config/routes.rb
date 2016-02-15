c95f57d794604695d01dfff1df90d74b5e6ea31d
schen35 <schen35@ncsu.edu>
2016-02-15 13:50:27 -0500

    prototype pre-alpha
    sign-in
    sign-out
    with role of user only

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root to: 'visitors#index'
+  devise_for :users
+
+
+
+  devise_scope :user do
+    get 'login', to: "devise/sessions#new", as: "login"
+    get 'logout', to: "devise/sessions#destroy", as: "logout"
+    get 'signout', to: "users/sessions#destroy", as: "signout"
+    get 'signup', to: "users#new", as: "signup"
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
+
+  resources :users
+end
