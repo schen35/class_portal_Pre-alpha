cee889771974eb77f3e00089346f392c02f3439c
schen35 <schen35@ncsu.edu>
2016-02-16 17:56:13 -0500

    !before bad things

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-
   #root to: 'users#index'
   root to: 'visitors#index'
   devise_for :users
@@ -15,6 +13,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
+
+
   resources :users
-  resources :admin
 end
