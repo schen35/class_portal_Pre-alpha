83595b8953075b6cf680c9564b43db9143e0c781
Harold Chen <ychen71@ncsu.edu>
2016-02-18 15:41:14 -0500

    Update database to postgreSQL

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 #ruby '~> 2.2.1'
 gem 'rails', '4.2.5.1'
-gem 'sqlite3'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -10,6 +10,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'autoprefixer-rails'
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'cancancan'
+gem 'rails_12factor', group: :production
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160217042658) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.integer  "Course_num"
     t.string   "Title"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160217042658) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "courses", ["Course_num"], name: "index_courses_on_Course_num", unique: true
+  add_index "courses", ["Course_num"], name: "index_courses_on_Course_num", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 20160217042658) do
     t.integer  "role"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
