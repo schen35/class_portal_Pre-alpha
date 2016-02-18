c11760faea3b5e7e572d770da9a86779b25a2ca5
Harold Chen <ychen71@ncsu.edu>
2016-02-18 14:03:08 -0500

    ~~Delete function success~~

--- app/controllers/superadmins_controller.rb
+++ app/controllers/superadmins_controller.rb
@@ -1,3 +1,11 @@
 class SuperadminsController < UsersController
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   respond_to do |format|
+  #     @user.destroy
+  #     format.html { redirect_to users_path, notice: 'This user was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,9 +49,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to user_path, notice: 'This user was successfully destroyed.' }
+      format.html { redirect_to users_path, notice: 'This user was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
