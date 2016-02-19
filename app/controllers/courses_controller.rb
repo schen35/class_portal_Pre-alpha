3b362d162d9a528c1492cf48362dc5f155855821
Harold Chen <ychen71@ncsu.edu>
2016-02-18 19:38:52 -0500

    update on heroku deployment

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -56,9 +56,10 @@ class CoursesController < ApplicationController
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
+    @course = Course.find(params[:id])
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.html { redirect_to courses_path, notice: 'Course was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
