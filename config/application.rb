1aa513f43be18f8ad919afb8e85953f787587acc
Harold Chen <ychen71@ncsu.edu>
2016-02-18 19:38:52 -0500

    update on heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module RailsDevise
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.enabled = false
   end
 end
 module Register
