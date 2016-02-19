4a65b9ba91bac6d398407ef2b658a8ed79406732
Harold Chen <ychen71@ncsu.edu>
2016-02-18 19:38:52 -0500

    update on heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,6 +23,7 @@ module RailsDevise
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.enabled = false
   end
 end
 module Register
