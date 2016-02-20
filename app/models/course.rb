2783af5c513c9e78b8327c0a01e2bc3c3184f61e
Harold Chen <ychen71@ncsu.edu>
2016-02-19 20:07:31 -0500

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,6 @@
 class Course < ActiveRecord::Base
   has_many :enrollments
   has_many :user, through: :enrollments
-<<<<<<< HEAD
-  # validates_presence_of :course_id
-  # validates_uniqueness_of :course_id
-  # validates_presence_of :start_date
-  # validates_presence_of :end_date
-
   def self.search(search)
     if search
       self.where('"courses"."Title" LIKE ?', "%#{search}%")
@@ -15,10 +9,5 @@ class Course < ActiveRecord::Base
     end
 
   end
-=======
-  validates_presence_of :course_id
-  validates_uniqueness_of :course_id
-  validates_presence_of :start_date
-  validates_presence_of :end_date
->>>>>>> origin/devarenacn
+
 end
